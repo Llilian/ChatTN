2e23366a765aca73dae64eac5efed7d4d9a1f500
Lilian <lilian.lemee@hotmail.fr>
2017-11-08 09:27:51 +0100

    Maj README + plan

--- IChambre6/ListUsersTableViewController.m
+++ IChambre6/ListUsersTableViewController.m
@@ -18,9 +18,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     _mCManager = [[MultipeerConnectionManager alloc] init];
-    
+
     MCNearbyServiceAdvertiser *advertiser = [_mCManager Initialization:_myRoom];
     [advertiser setDelegate:self];
 }
@@ -32,7 +32,7 @@
 
 - (IBAction)ResearchUsers:(id)sender {
     [_mCManager BrowserConnection:[self myRoom]];
-    
+
     [self presentViewController: [_mCManager browserViewController]
                        animated:YES completion:^{
                            [[_mCManager browser] startBrowsingForPeers];
@@ -41,7 +41,7 @@
 }
 
 - (IBAction)Synchronize:(id)sender {
-    
+
     [[self tableView] reloadData];
 }
 
@@ -57,9 +57,9 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"item" forIndexPath:indexPath];
-    
+
     [[cell textLabel] setText:[[[[self myRoom] UsersAccepted] objectAtIndex:indexPath.row] displayName]];
-    
+
     return cell;
 }
 
@@ -79,7 +79,7 @@
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+    }
 }
 */
 
@@ -116,36 +116,36 @@ didReceiveInvitationFromPeer:(MCPeerID *)peerID
       withContext:(NSData *)context
 invitationHandler:(void (^)(BOOL accept, MCSession *session))invitationHandler
 {
-    
+
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Connexion entrante"
                                                                    message: [[peerID displayName] stringByAppendingString:@" veut discuter avec vous !"]
                                                             preferredStyle:UIAlertControllerStyleAlert];
-    
+
 //    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Connexion entrante"
 //                                                                   message:@"Quelqu'un veut discuter avec vous !"
 //                                                            preferredStyle:UIAlertControllerStyleAlert];
-    
+
     UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Accepté" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action)
                          {
                              [_myRoom addUser:peerID andStatus:(bool *) true];
                              invitationHandler(YES, [_mCManager mySession]);
                          }];
-    
+
     UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Rejeté" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action)
                              {
                                  [_myRoom addUser:peerID andStatus:(bool *) false];
                                  invitationHandler(NO, nil);
                              }];
-    
+
     [alert addAction:cancel];
     [alert addAction:ok];
-    
+
     /*[self dismissViewControllerAnimated:YES completion:^{
         nil;
     }];*/
-    
+
     [self presentViewController:alert animated:YES completion:nil];
-    
+
 }
 
 #pragma mark - MCSessionDelegate
@@ -154,7 +154,16 @@ invitationHandler:(void (^)(BOOL accept, MCSession *session))invitationHandler
     didReceiveData:(NSData *)data
     fromPeer:(MCPeerID *)peerID
 {
-    
+    // Sauvegarde dans fichier Json conversation ( Pour avoir plusieurs conversation même temps)
+    // Si chat avec 1 : moi écrit : affiche dans fenêtre et sauvegarde dans fichier Json
+    //  1 écrit : affiche et write json file
+    // Je quitte chat avec 1 et chat avec 2 :
+    //  Si 1 écrit, n'affiche pas (pas dans son chat) mais enregistre (Me notifie ?).
+    // Quand je reviens chat avec lui : charge conversation du Json file
+
+    // Quand je quitte l'application : 2 choix :
+    //  * Supprime conversation existante
+    //  * Garde conversation existante + affiche aux prochaines connexion (Mais normalement impossible de lié à un PeerID).
 }
 
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ChatTN
+
+##Done
+* Recherche d'autre équipement
+* Connexion à un autre équipement
+
+##TODO
+* Enregistrement des messages dans des fichiers Json
+* Gestion de plusieurs discussions
+* Gestion des listes Accepted et Rejected
+* Possibilité de relire ancienne conversation ?
+
+##Doing
+* Envoie d'un message
+* Reception d'un message
